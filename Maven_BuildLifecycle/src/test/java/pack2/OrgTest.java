7a4b80b8f73a2ec373d7c8ec4bf8aaedda68a1c1
Bikash Kumar Jha <bikashj038@gmail.com>
2024-07-10 18:14:30 +0530

    new update for delete

--- Maven_BuildLifecycle/src/test/java/pack2/OrgTest.java
+++ Maven_BuildLifecycle/src/test/java/pack2/OrgTest.java
@@ -4,15 +4,20 @@ import org.testng.annotations.Test;
 
 public class OrgTest 
 {
-	@Test(groups = "ST")
+	@Test(groups = "ST" )
 	public void createOrgTest()
 	{
 		System.out.println("execute createOrgTest");
 	}
 	
-	@Test(groups = "RT")
+	@Test(groups = "RT" )
 	public void modifyOrgTest()
 	{
 		System.out.println("execute modifyOrgTest");
+	}
+	@Test
+	public void deleteOrgTest()
+	{
+		System.out.println("execute deleteOrgTest");
 	}
 }
